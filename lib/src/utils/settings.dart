a61583ae4992fd1b64b5ff4c6ce2ee85cfad5076
vijaysharmapc <vijaysharmapc@gmail.com>
2021-05-29 23:33:23 -0400

    Delete settings.dart

--- lib/src/utils/settings.dart
+++
@@ -1,4 +0,0 @@
-/// Define App ID and Token
-const APP_ID = 'd5c92e10cc6a4e11b79c9a10c71f711d';
-const Token =
-    '006d5c92e10cc6a4e11b79c9a10c71f711dIACxoP9eojOWvJGRfCyhHEcDUt0ket1K3bDjS/H2U/vqPhX89r8AAAAAEADEZWnpTvGyYAEAAQBN8bJg';
