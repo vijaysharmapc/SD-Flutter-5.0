f3adaab9595a2183ebf04054922cabbbfe3b744b
Vijay Sharma <vijaysharmapc@gmail.com>
2021-05-29 23:20:23 -0400

    First commit

--- android/app/src/main/java/com/example/agoraflutterquickstart/MainActivity.java
+++ android/app/src/main/java/com/example/agoraflutterquickstart/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.agoraflutterquickstart;
+package com.example.sdflutterquickstart;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,4 @@
 /// Define App ID and Token
-const APP_ID = Your_App_ID;
-const Token = Your_Token;
+const APP_ID = 'd5c92e10cc6a4e11b79c9a10c71f711d';
+const Token =
+    '006d5c92e10cc6a4e11b79c9a10c71f711dIACxoP9eojOWvJGRfCyhHEcDUt0ket1K3bDjS/H2U/vqPhX89r8AAAAAEADEZWnpTvGyYAEAAQBN8bJg';
